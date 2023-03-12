3595c8dd6b745512b661d1e864678e05fb13a443
tri222 <tritiya914.be22@chitkara.edu.in>
2023-03-12 13:04:46 +0530

    initial

---
+++ program1.c
@@ -0,0 +1,22 @@
+int main()
+{
+int a,b,c;
+scanf("%d",&a);	
+scanf("%d",&b);	
+scanf("%d",&c);	
+
+if (a>c && a>b )
+{ 
+ printf("a is greater");
+}
+
+else if (b>c && b>a)
+{
+
+ printf("b is greater");
+}
+else
+{
+	printf("c is greater");
+}
+}
